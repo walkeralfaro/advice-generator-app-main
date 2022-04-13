a8a08508021bb9ac7c00f99fd8887cc667ee4750
Walker Alfaro Trelles <alfaro.trelles@gmail.com>
2022-04-13 01:46:57 -0500

    upgrade script JS

--- js/app.js
+++ js/app.js
@@ -2,19 +2,26 @@ const   btnAPI = document.querySelector('.advice__button'),
         adviceText = document.querySelector('.advice__text--p'),
         adviceNum = document.querySelector('.advice__number');
 
-btnAPI.addEventListener('click', (e) => {
+window.onload = function() {
+    getAdvice();
+}
+
+btnAPI.addEventListener('click', function() {
+    getAdvice();
+})
+
+function getAdvice() {
     const id = randomNum();
     const url = `https://api.adviceslip.com/advice/${id}`;
 
     fetch(url)
         .then( response => response.json() )
-        .then( advice => drawAdvice(advice) )
+        .then( adviceData => drawAdvice(adviceData.slip) )
         .catch( error => console.log(error.message) )
-})
+}
 
-function drawAdvice(quote) {
-    const { slip } = quote;
-    const { id, advice } = slip;
+function drawAdvice(phrase) {
+    const { id, advice } = phrase;
 
     adviceNum.textContent = id;
     adviceText.textContent = `"${advice}"` ;
